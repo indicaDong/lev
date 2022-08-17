0407444ebaf151f2eb9cb6dcda7c5affdb33faa4
Dong Xavier <1853155@tongji.edu.cn>
2022-08-18 00:51:40 +0800

    test4

--- get_shap_values.py
+++ get_shap_values.py
@@ -11,16 +11,38 @@ checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
 
 model = TFAutoModelForSequenceClassification.from_pretrained(checkpoint)
 
-tokenizer = AutoTokenizer.from_pretrained(checkpoint, max_lenth = 1024)
+tokenizer = AutoTokenizer.from_pretrained(checkpoint, model_max_length = 1024)
 classifier  = transformers.pipeline("sentiment-analysis", model=model, tokenizer=tokenizer,  return_all_scores=True)
 
 
 
+# def get_shap_values(out ,SRC ,EOS_WORD ='</s>'):
+#     src_sentences = [vector_to_sentence(out[i, :], SRC, EOS_WORD, start_from=0) for i in range(out.size(0))]
+#     sentence = [" ".join(src_sentences)]
+#     print(len(tokenizer(sentence)["input_ids"][]))
+#     explainer = shap.Explainer(classifier)
+#     print(sentence)
+#     shap_values = explainer(sentence)
+#     #score = classifier(sentence)
+#     s_values = torch.tensor(shap_values.values)
+#     print(s_values.size())
+   
+#    # print(score)
+#     return s_values #,score
+
 def get_shap_values(out ,SRC ,EOS_WORD ='</s>'):
     src_sentences = [vector_to_sentence(out[i, :], SRC, EOS_WORD, start_from=0) for i in range(out.size(0))]
-    sentence = [" ".join(src_sentences)]
-    print(len(tokenizer(sentence)["input_ids"]))
+    tokens = []
+    for i in range(out.size(0)):
+      for j in i:
+        tokens.append(vector_to_sentence(j, SRC, EOS_WORD, start_from=0 ))
+    print("tokens",len(tokens))
+
+
+    sentence = [" ".join(tokens)]
+    #print(len(tokenizer(sentence)["input_ids"][0]))
     explainer = shap.Explainer(classifier)
+    
     print(sentence)
     shap_values = explainer(sentence)
     #score = classifier(sentence)
